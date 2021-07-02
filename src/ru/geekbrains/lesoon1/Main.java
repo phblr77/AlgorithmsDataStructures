2fcb91407b24c6d3f858d8416664d1af504a0c46
krukd <krukd87@gmail.com>
2021-07-02 17:33:54 +0200

    Initial commit

---
+++ src/ru/geekbrains/lesoon1/Main.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lesoon1;
+
+
+//Первое ДЗ было просто прочитать книгу
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Algorithms course java ");
+    }
+}
